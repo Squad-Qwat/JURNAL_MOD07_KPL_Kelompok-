20697a0fe7818222d13279f63c8597ec8523d664
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-04-09 07:16:45 +0700

    menambahkan class DataMahasiswa103022300121

---
+++ DataMahasiswa103022300121.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Jurnal07_Kelompok
+{
+    class DataMahasiswa103022300121
+    {
+        public class Address
+        {
+            public string streetAddress { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+        }
+
+        public class Course
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+
+
+
+        public class Mahasiswa
+        {
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public string gender { get; set; }
+            public int age { get; set; }
+            public Address address { get; set; }
+            public List<Course> courses { get; set; }
+        }
+
+        public void ReadJSON(string filePath)
+        {
+
+            string json = File.ReadAllText(filePath);
+            Mahasiswa data = JsonSerializer.Deserialize<Mahasiswa>(json);
+            Console.WriteLine("Nama " + data.firstName + " " + data.lastName);
+            Console.WriteLine("Gender " + data.gender);
+            Console.WriteLine("Umur " + data.age);
+            Console.WriteLine("===Alamat=== " + data.age);
+            Console.WriteLine("Jalan: " + data.address.streetAddress);
+            Console.WriteLine("Kota/Kab: " + data.address.city);
+            Console.WriteLine("Provinsi: " + data.address.state);
+            Console.WriteLine("===Mata Kuliah===");
+            int i = 1;
+            foreach (var course in data.courses)
+            {
+                Console.WriteLine("MK " + i + " " + course.code + " - " + course.name);
+                i++;
+            }
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Jurnal07_Kelompok;
+
+DataMahasiswa103022300121 dataMahasiswa103022300121 = new DataMahasiswa103022300121();
+
+dataMahasiswa103022300121.ReadJSON("jurnal7_1_103022300121.json");
