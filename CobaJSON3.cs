75cef3719891670f712d4f4bf0d48b9836a21e35
Tdarrelpa <darrel2208@gmail.com>
2025-04-09 07:52:05 +0700

    menambahkan class GlossaryItem_103022300082

---
+++ CobaJSON3.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace CobaJSON3
+{
+    public class glossdef 
+    {
+        public string[] glossseealso { get; set; }
+        public string para { get; set; }
+    }
+
+    public class glossentry
+    {
+        public string id { get; set; }
+        public string sortas { get; set; }
+        public string glossterm { get; set; }
+        public string[] glossdef { get; set; }
+        public string Acronym { get; set; }
+        public string glossabbrev { get; set; }
+        public string glosssee { get; set; }
+    }
+
+    public class glosslist
+    {
+        public string title { get; set; }
+        public string[] glossentry { get; set; }
+    }
+
+    public class glossdiv
+    {
+        public string title { get; set; }
+        public string[] glosslist { get; set; }
+    }
+
+    public class glossary
+    {
+        public string title { get; set; }
+        public string[] glossdiv { get; set; }
+    }
+
+    public class GlossaryItem_103022300082
+    {
+        public List<glossary> glossaries { get; set; }
+
+        public static GlossaryItem_103022300082 ReadJSON(string fileName)
+        {
+            string jsonString = File.ReadAllText(fileName);
+            return JsonSerializer.Deserialize<GlossaryItem_103022300082>(jsonString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CobaJSON1;
+using CobaJSON2;
+using CobaJSON3;
 
 static void Main(string[] args)
 {
     string fileName = "jurnal7_1_103022300082.json";
     DataMahasiswa_103022300082 data = DataMahasiswa_103022300082.ReadJSON(fileName);
     data.PrintData();
+
+    string fileName2 = "jurnal7_2_103022300082.json";
+    TeamMembers_103022300082.ReadJSON(fileName2);
+
+    string fileName3 = "jurnal7_3_103022300082.json";
+    TeamMembers_103022300082.ReadJSON(fileName3);
 }
