30872a42d392b26f3f1766cf9f3b92eff34e464b
Muhammad abiyyu <abiyyurahid20@gmail.com>
2025-04-09 08:00:09 +0700

    menambahkan class GlossaryItem103022300121

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 DataMahasiswa103022300121 dataMahasiswa103022300121 = new DataMahasiswa103022300121();
 TeamMembers103022300121 teamMembers103022300121 = new TeamMembers103022300121();
+GlossaryItem103022300121 glossaryItem103022300121 = new GlossaryItem103022300121();
 
 dataMahasiswa103022300121.ReadJSON("jurnal7_1_103022300121.json");
 teamMembers103022300121.ReadJSON("jurnal7_2_103022300121.json");
+glossaryItem103022300121.ReadJSON("jurnal7_3_103022300121.json");
+
